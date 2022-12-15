1e8678538b72734ec1746fa45c3ac320aeb8c520
stoo johnston <scj@stooj.org>
2022-12-15 21:23:56 +0000

    feat(tests): Add clean test

---
+++ test/integration/clean/controls/config.rb
@@ -0,0 +1,50 @@
+# # frozen_string_literal: true
+
+control 'saltplusplus ssh key symlink' do
+  title 'should not exist'
+
+  describe file('/srv/salt/ssh/files/deploy_key') do
+    it { should_not exist }
+  end
+end
+
+control 'saltplusplus ssh file root directory' do
+  title 'should not exist'
+
+  describe directory('/srv/salt/ssh/files') do
+    it { should_not exist }
+  end
+end
+
+control 'saltplusplus ssh key' do
+  title 'should not exist'
+
+  describe file('/root/.ssh/id_rsa') do
+    it { should_not exist }
+  end
+end
+
+control 'saltplusplus ssh public key' do
+  title 'should not exist'
+
+  describe file('/root/.ssh/id_rsa.pub') do
+    it { should_not exist }
+  end
+end
+
+control 'saltplusplus ssf git script' do
+  title 'should not exist'
+
+  describe file('/srv/salt/ssf/files/fishers/git/git_30_create_PR.sh') do
+    it { should_not exist }
+  end
+end
+
+control 'saltplusplus ssf file root directory' do
+  title 'should not exist'
+
+  describe directory('/srv/salt/ssf/files/fishers/git') do
+    it { should_not exist }
+  end
+end
+
